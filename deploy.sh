4bb12eb685f32b805a9ca722cb3fd3ce5ee2a88b
Olya Severova <34347298+olyaseverova@users.noreply.github.com>
2024-06-15 18:34:05 +0300

    'withdist'

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,13 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-echo 'romanov.ru' > CNAME
+echo 'vladimirromanov.art' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:olyaseverova/romanov.git main:gh-pages
+git push -f git@github.com:olyaseverova/romanov.git master:gh-pages
 
 cd -
